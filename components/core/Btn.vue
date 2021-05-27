3210e4001ce0682c5644b2423d9734076d9565ad
Matheus Pedroni <mpedroni77@gmail.com>
2021-05-27 16:49:24 -0300

    feat(core components): create the standard Button component

---
+++ components/core/Btn.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn
+    v-bind="$attrs"
+    text
+    outlined
+    color="primary"
+    @click="$emit('click', $event)"
+  >
+    <v-icon v-if="icon" left v-text="icon" />
+    {{ label }}
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CoreButton',
+
+  props: {
+    icon: {
+      type: String,
+      default: null,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
